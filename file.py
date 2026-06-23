b3ccf21ff0fb9a6ac5a77e6d686a3f4e7273ca12
Lulu Mohamed <lulumohamed@Lulus-MacBook-Air.local>
2026-06-23 01:35:32 -0700

    lulu

--- file.py
+++ file.py
@@ -1,5 +1,5 @@
 file = open("notes.txt", "a")
 file.write("My name is lulu")
 file.write("\nI am a student")
-file.write("\nI am 20 years old")
+file.write("\nI am 20 years old")#this is not important
 file.close()
